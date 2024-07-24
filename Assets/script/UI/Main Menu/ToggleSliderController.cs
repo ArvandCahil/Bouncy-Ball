9041b09ea8cf047ca0c4b8464cb954c140a34f42
Kenzoku00 <155788641+Kenzoku00@users.noreply.github.com>
2024-07-24 16:55:14 +0700

    Inventory Slider ( 1 /2 )
    pls sabar kan dulu le...

---
+++ Assets/script/UI/Main Menu/ToggleSliderController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSliderController : MonoBehaviour
+{
+    public RectTransform slider;  
+    public RectTransform upperLimitObject; 
+    public float speed = 5f;     
+    public float lowerLimit = -100f; 
+
+    private bool isMoving = false;
+    private bool moveUp = true;
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            float moveAmount = speed * Time.deltaTime * (moveUp ? 1 : -1);
+            Vector3 newPosition = slider.localPosition + new Vector3(0, moveAmount, 0);
+
+            float upperLimit = upperLimitObject.localPosition.y;
+
+            if (newPosition.y > upperLimit)
+            {
+                newPosition.y = upperLimit;
+                isMoving = false;
+                moveUp = false; 
+            }
+            else if (newPosition.y < lowerLimit)
+            {
+                newPosition.y = lowerLimit;
+                isMoving = false;
+                moveUp = true; 
+            }
+
+            slider.localPosition = newPosition;
+        }
+    }
+
+    public void ToggleSlider()
+    {
+        isMoving = !isMoving;
+    }
+}
