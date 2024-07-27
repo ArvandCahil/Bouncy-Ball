c9ccacbef3e1b858b1461f2754e8c39c19feec11
kat <165371226+Farizzzz27@users.noreply.github.com>
2024-07-28 02:12:23 +0700

    Create a ball bounce counter

--- Assets/script/Gameplay/Ball Rotation.cs
+++ Assets/script/Gameplay/Ball Rotation.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +6,8 @@ public class BallRotation : MonoBehaviour
 {
     private float torqueMultiplier = 0.005f;
     private Rigidbody rb;
+    public UnitController controller;
+    private bool canMove = true; 
 
     void Start()
     {
@@ -15,21 +17,43 @@ public class BallRotation : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+
         // Get the contact point of the collision
         ContactPoint contact = collision.contacts[0];
 
-        // Calculate the direction of the torque based on the contact point and velocity
-        Vector3 torqueDirection = Vector3.Cross(contact.normal, rb.velocity).normalized;
+        if (controller.moveCount < controller.maxMoves)
+        {
+            controller.moveCount++;
+            controller.updateText();
+        }
+        else
+        {
+            Debug.Log("Max moves reached.");
+            canMove = false;
+        }
 
-        // Apply the torque to the ball
-        rb.AddTorque(torqueDirection * rb.velocity.magnitude * torqueMultiplier, ForceMode.Impulse);
+        if (canMove)
+        {
+            ApplyTorque(collision);
+        }
+        Debug.Log("Can move: " + canMove);
     }
 
     void OnCollisionStay(Collision collision)
     {
-        // Continuously apply torque while the ball is in contact with a surface
+        if (canMove)
+        {
+            ApplyTorque(collision);
+        }
+    }
+
+    private void ApplyTorque(Collision collision)
+    {
+        // Calculate the direction of the torque based on the contact point and velocity
         ContactPoint contact = collision.contacts[0];
         Vector3 torqueDirection = Vector3.Cross(contact.normal, rb.velocity).normalized;
+
+        // Apply the torque to the ball
         rb.AddTorque(torqueDirection * rb.velocity.magnitude * torqueMultiplier, ForceMode.Force);
     }
 }
